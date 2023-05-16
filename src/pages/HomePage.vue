a24f6cf5106e75caae60fc4657953706dbcfd604
Martin Neumann <ma-neumann@outlook.com>
2023-05-16 15:28:28 +0200

    HomePage refinement

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,35 +1,19 @@
 <template>
 
   <div class="home">
-    <div class="welcome-container">
-      <h2>Willkommen, {{ username }}!</h2>
-    </div>
-    <v-card class="survey-card">
-      <v-card-title>Meine Umfragen</v-card-title>
-      <v-card-text>
-        <div v-if="createdSurveys.length === 0">
-          Du hast noch keine Umfragen erstellt.
-        </div>
-        <ul v-else>
-          <li v-for="survey in createdSurveys" :key="survey.id">{{ survey.title }}</li>
-        </ul>
-      </v-card-text>
-    </v-card>
-    <v-card class="survey-card">
-      <v-card-title>Eingeladene Umfragen</v-card-title>
-      <v-card-text>
-        <div v-if="invitedSurveys.length === 0">
-          Du wurdest zu keiner Umfrage eingeladen.
-        </div>
-        <ul v-else>
-          <li v-for="survey in invitedSurveys" :key="survey.id">{{ survey.title }}</li>
-        </ul>
-      </v-card-text>
-    </v-card>
-    <v-btn @click="createSurvey" color="primary" icon>
-      <v-icon>mdi-plus</v-icon>
-      Umfrage erstellen
-    </v-btn>
+
+    <h1>Willkommen, {{ username }}!</h1>
+
+    <v-sheet elevation="1">
+      <h2>Meine Umfragen</h2>
+      <p>Du hast noch keine Umfragen erstellt.</p>
+    </v-sheet>
+
+    <v-sheet elevation="1">
+      <h2>Eingeladene Umfragen</h2>
+      <p>Du wurdest zu keiner Umfrage eingeladen.</p>
+    </v-sheet>
+
   </div>
   
 </template>
@@ -37,42 +21,36 @@
 <script setup>
 
     import { ref } from 'vue';
-    import { useRouter } from 'vue-router';
 
-    const username = ref('John Doe'); // Hier den tatsächlichen Benutzernamen setzen
-    const createdSurveys = ref([]); // Hier die tatsächliche Liste der selbst erstellten Umfragen setzen
-    const invitedSurveys = ref([]); // Hier die tatsächliche Liste der eingeladenen Umfragen setzen
-
-    const router = useRouter();
-
-    function createSurvey() {
-        // Hier die Logik hinzufügen, um zur UmfragePage weiterzuleiten
-        router.push('/umfrage-erstellen');
-    }
+    const username = ref('John Doe');
+    // const createdSurveys = ref([]);
+    // const invitedSurveys = ref([]);
 
 </script>
 
 <style scoped>
 
     .home {
-        max-width: 800px;
-        margin: 0 auto;
-        padding: 20px;
+      max-width: 75%;
+      margin: 0 auto;
+      padding: 20px;
     }
 
-    .welcome-container {
-        display: flex;
-        justify-content: center;
-        margin-bottom: 20px;
+    .v-sheet {
+      margin-bottom: 20px;
+      padding: 10px;
     }
 
-    .survey-card {
-        margin-bottom: 20px;
+    .v-sheet h2 {
+      font-weight: 500;
+      font-size: large;
+      padding-bottom: 10px;
     }
 
-    h2 {
-        text-align: left;
-        margin-bottom: 10px;
+    h1 {
+      text-align: left;
+      margin: 20px 0 20px 0;
+      font-weight: 400;
     }
 
 </style>
